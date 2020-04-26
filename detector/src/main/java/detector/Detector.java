3e7f4cb1883e0c787d494d62c2c950e79fb0a246
Louis Antonini <louis.antonini@epitech.eu>
2020-04-26 12:12:03 +0200

    Set detector Kafka stream topology to sink scan data in separate topics

--- detector/src/main/java/detector/Detector.java
+++ detector/src/main/java/detector/Detector.java
@@ -2,7 +2,10 @@ package detector;
 
 import lombok.extern.slf4j.Slf4j;
 import types.HttpResponseDigest;
+import types.HttpServer;
+import types.ServerScan;
 
+import java.util.Arrays;
 import java.util.Properties;
 
 import org.apache.kafka.common.serialization.Serdes;
@@ -11,25 +14,59 @@ import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.Topology;
 import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.ValueMapper;
 
 import io.confluent.kafka.serializers.AbstractKafkaSchemaSerDeConfig;
 import io.confluent.kafka.streams.serdes.avro.SpecificAvroSerde;
 
+/**
+ * A Kafka streaming application to identify the technology used by a server.
+ * The Detector uses scraping-data topic HttpResponseDigest serialized data as source
+ */
 @Slf4j
 public class Detector {
     private static String SCRAPING_DATA_TOPIC = "scraping-data";
-    private static String DETECTION_DATA_TOPIC = "detection-data";
+    private static String HTTP_SERVER_DATA = "http-server-data";
+    private static String SERVER_SCAN_DATA = "server-scan-data";
     private static String BROKER_HOST = System.getenv("BROKER_HOST");
     private static String BROKER_PORT = System.getenv("BROKER_PORT");
     private static String SCHEMA_REGISTRY_HOST = System.getenv("SCHEMA_REGISTRY_HOST");
 
     public static void main(String[] args) {
         StreamsBuilder builder = new StreamsBuilder();
-        KStream<String, HttpResponseDigest> stream = builder.stream(SCRAPING_DATA_TOPIC);
-        stream.peek((key, value) -> log.info(String.format("HttpResponseDigest for %s: %s", key, value.toString())))
-            .to(DETECTION_DATA_TOPIC);
-        Topology topology = builder.build();
 
+        // Build http server stream
+        KStream<String, HttpResponseDigest> stream = builder.stream(SCRAPING_DATA_TOPIC);
+        stream
+            .peek((key, value) -> log.info(String.format("HttpResponseDigest for %s: %s", key, value.toString())))
+            .flatMapValues(new ValueMapper<HttpResponseDigest, Iterable<HttpServer>> (){
+                @Override
+                public Iterable<HttpServer> apply(HttpResponseDigest value) {
+                    return Arrays.asList(HttpServer.newBuilder()
+                        .setId((long)42)
+                        .setHost("HOST")
+                        .setIp("IP")
+                        .build());
+                }
+            })
+            .to(HTTP_SERVER_DATA);
+
+        // Build server scan topology
+        stream
+            .peek((key, value) -> log.info(String.format("HttpResponseDigest for %s: %s", key, value.toString())))
+            .flatMapValues(new ValueMapper<HttpResponseDigest, Iterable<ServerScan>> (){
+                @Override
+                public Iterable<ServerScan> apply(HttpResponseDigest value) {
+                    return Arrays.asList(ServerScan.newBuilder()
+                        .setId((long)84)
+                        .setHttpServerId((long)84)
+                        .setTechnology("TECHNOLOGY")
+                        .build());
+                }
+            })
+            .to(SERVER_SCAN_DATA);
+
+        Topology topology = builder.build();
         Properties props = new Properties();
         props.put(StreamsConfig.APPLICATION_ID_CONFIG, "detector");
         props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, BROKER_HOST + ":" + BROKER_PORT);
